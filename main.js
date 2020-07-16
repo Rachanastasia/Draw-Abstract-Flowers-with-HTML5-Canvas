bfff95029e1b8d98027d98d3cba75832dcc7208c
Rachel A Reilly <46611223+Rachanastasia@users.noreply.github.com>
2020-07-16 12:42:08 -0500

    Add files via upload
    Added comments, fixed saturation gradual transition;

--- main.js
+++ main.js
@@ -6,29 +6,41 @@ ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 85;
 
+/*var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "white");
+
+// Fill with gradient
+ctx.fillStyle = grd;
+ctx.fillRect(10, 10, 150, 80);*/
+
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let direction = true;
-let dir = false;
-
+let dir = true;
 let saturation = 90;
 let lightness = 50;
 
+//starts drawing point in center
 let centerWidth = (window.innerWidth) / 2;
 let centerHeight = (window.innerHeight) / 2;
 
-let colors = [345, 235, 305, 4];
+//how can I choose the hue from this array of colors 
+//from within the template string?
+//let colors = [345, 235, 305, 4];
 
 
 function draw(e){
   if (!isDrawing) return;
   console.log(e);
-  ctx.strokeStyle = `hsl(4, ${saturation}%, ${lightness}%)`;
+  ctx.strokeStyle = `hsl(305, ${saturation}%, ${lightness}%)`;
   ctx.beginPath();
 
   ctx.moveTo(lastX, lastY);
 
+  //arc creates a circular motion
+  //variables change center point to center of window
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.arc(centerWidth, centerHeight, 40, 12, 2 * Math.PI);
   ctx.stroke();
@@ -38,17 +50,7 @@ function draw(e){
 
 
 
-/*
-  saturation++;
-  if (saturation >=90){
-    saturation--;
-  }*/
-
- /* lightness++;
-  if (lightness >=90){
-    lightness = 10;
-  }*/
-
+ //gradual change in lightness value
   if (lightness >= 55 || lightness <=20){
     direction = !direction;
 
@@ -61,7 +63,7 @@ function draw(e){
   }
 
 
-
+//gradual change in saturation value
 if (saturation >= 80 || saturation <=50){
     dir = !dir;
 
@@ -72,6 +74,8 @@ if (saturation >= 80 || saturation <=50){
   } else {
     saturation--;
   }
+
+  
 }
 
 
